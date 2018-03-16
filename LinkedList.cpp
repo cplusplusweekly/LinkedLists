01ee0c16567f27f1505a21b36d8eb2cbd6b01500
dpollan <davidpollan@gmail.com>
2018-03-15 20:20:14 -0500

    Implements function to display head of Linked List

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <array>
-#include <map>
+
 using namespace std;
 
 class Node {
@@ -18,6 +17,7 @@ public:
 		tail=NULL;
 	}
 	void insertNode(int number);
+	int displayHead();
 };
 
 
@@ -41,12 +41,20 @@ void LinkedList::insertNode(int number){
 	}
 }
 
+int LinkedList::displayHead(){
+	return head->data;
+
+}
+
 
 int main()
 {
 	cout << "Creating Linked List:\n";
-	LinkedList();
+	LinkedList ll;
 	cout << "Linked List Created!\n";
+	cout << "Adding number 1 as head\n";
+	ll.insertNode(1);
+	cout << "head is equal to " << ll.displayHead() << endl;
 
 	return 0;
 }
